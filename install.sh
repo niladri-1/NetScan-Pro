8bf74f81ddda75cf1c2d7ab10805201d61ab5819
Niladri Chatterjee <code.niladri@gmail.com>
2025-04-03 08:36:48 +0530

    Update dependencies

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 # Update package lists and install dependencies
 echo "Installing dependencies..."
-sudo apt-get install -y nmap traceroute figlet lolcat cowsay
+sudo apt-get install -y nmap traceroute figlet lolcat cowsay toilet nmcli iwlist iwconfig
 
 # Give execute permission to the main script
 chmod +x network_scan.sh
